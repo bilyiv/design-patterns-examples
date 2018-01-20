70cef7617e614d212f566213aee98f097177111a
bilyiv <beliyvladislav@gmail.com>
2018-01-21 01:42:12 +0200

    create percent discount

---
+++ StrategyPattern/Discount/PercentDiscount.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace StrategyPattern\Discount;
+
+/**
+ * Class PercentDiscount
+ *
+ * @package StrategyPattern\Discount
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class PercentDiscount extends AbstractDiscount
+{
+    /**
+     * @inheritdoc
+     */
+    public function calculate(float $amount): float
+    {
+        return $amount * $this->size / 100;
+    }
+}
