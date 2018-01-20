517c8d8bdc4b2641373ddcdeae1f778898e691d8
bilyiv <beliyvladislav@gmail.com>
2018-01-21 01:55:07 +0200

    create order class

---
+++ StrategyPattern/Order/Order.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace StrategyPattern\Order;
+
+use StrategyPattern\Discount\DiscountInterface;
+use StrategyPattern\Product\ProductInterface;
+
+/**
+ * Class Order
+ *
+ * @package StrategyPattern\Order
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class Order implements OrderInterface
+{
+    /**
+     * @var array
+     */
+    private $products = [];
+
+    /**
+     * @var float
+     */
+    private $total = 0.00;
+
+    /**
+     * @inheritdoc
+     */
+    public function addProduct(ProductInterface $product)
+    {
+        $this->products[] = $product;
+        $this->total += $product->price();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function total(): float
+    {
+        return $this->total;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function applyDiscount(DiscountInterface $discount)
+    {
+        $this->total -= $discount->calculate($this->total);
+    }
+}
