ba728f99b34bac05dba682e7730d9a28c4be5aa8
bilyiv <beliyvladislav@gmail.com>
2018-01-21 01:40:23 +0200

    create static discount

---
+++ StrategyPattern/Discount/StaticDiscount.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace StrategyPattern\Discount;
+
+/**
+ * Class StaticDiscount
+ *
+ * @package StrategyPattern\Discount
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class StaticDiscount extends AbstractDiscount
+{
+    /**
+     * @inheritdoc
+     */
+    public function calculate(float $amount): float
+    {
+        return $amount - $this->size;
+    }
+}
